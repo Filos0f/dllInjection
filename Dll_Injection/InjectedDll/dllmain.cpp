914cf9b396871401a6e532334164b894c909126a
pavelz <pavelz@example.ru>
2017-04-13 00:05:09 +0300

    Adding project

---
+++ Dll_Injection/InjectedDll/dllmain.cpp
@@ -0,0 +1,39 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "stdafx.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+	/*switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		MessageBoxA(0, "Hello", "", 0);
+	case DLL_THREAD_ATTACH:
+		MessageBoxA(0, "Hello", "", 0);
+	case DLL_THREAD_DETACH:
+		MessageBoxA(0, "Hello", "", 0);
+	case DLL_PROCESS_DETACH:
+		MessageBoxA(0, "Hello", "", 0);
+		break;
+	}*/
+	MessageBoxA(0, "RUNN!", 0, 0);
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		MessageBoxA(0, "Hello from Dll! PA", 0, 0);
+	case DLL_THREAD_ATTACH:
+		MessageBoxA(0, "Hello from Dll! TA", 0, 0);
+		
+	case DLL_THREAD_DETACH:
+		MessageBoxA(0, "Hello from Dll! TD", 0, 0);
+		
+	case DLL_PROCESS_DETACH:
+		MessageBoxA(0, "Hello from Dll! PD", 0, 0);
+		
+		break;
+	}
+	return TRUE;
+}
+
